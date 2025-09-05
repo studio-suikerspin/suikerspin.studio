b97b86fde6250550933f0cc6ce20d56ffcbd49bd
Robin de Laater <robin.delaater@shopmonkey.nl>
2025-09-05 12:02:54 +0200

    Add some meta

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,5 +3,14 @@ import ComingSoon from '@/components/sections/ComingSoon.vue'
 </script>
 
 <template>
+    <Head>
+        <title>Studio Suikerspin</title>
+        <meta name="description" content="Studio Suikerspin is een creatieve studio die zich richt op het creëren digitale producten.">
+        <meta name="keywords" content="Studio Suikerspin, creatief, digitale producten, design, development">
+        <meta name="author" content="Studio Suikerspin">
+        <meta name="robots" content="index, follow">
+        <meta name="googlebot" content="index, follow">
+    </Head>
+
     <ComingSoon />
 </template>
